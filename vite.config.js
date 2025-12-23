1a321bb264c0a1e8fe1ab60b7d0e8d7f724583ba
Kalmesh <kalmesh@chiplogictech.com>
2025-12-23 16:47:56 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     // Basic configuration
-    base: './',
+    base: '/social_check/',
     root: './',
     build: {
         outDir: 'dist',
